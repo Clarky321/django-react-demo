1021f5c9fe2ec862a32aa2171edf49be1e007c8f
Рябов Сергей Дмитриевич <ryabov-sd@ranepa.ru>
2024-11-01 14:41:41 +0300

    added notes backend

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import Note
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -48,3 +49,10 @@ class UserSerializer(serializers.ModelSerializer):
         )
 
         return user
+
+
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ["id", "title", "content", "created_at", "author"]
+        extra_kwargs = {"author": {"read_only": True}}
